27b2c97147341c165421f094567e206dee2f28f3
alexandre-guerra <alexsguerra@gmail.com>
2024-05-19 11:30:56 -0300

    refatorando o código

--- Files/utils.py
+++ Files/utils.py
@@ -1,8 +1,10 @@
-def test_stationarity(timeseries):
-    import pandas as pd
-    import plotly.graph_objs as go
-    from statsmodels.tsa.stattools import adfuller
+import pandas as pd
+import plotly.graph_objs as go
+from statsmodels.tsa.stattools import adfuller
+import yfinance as yf
+import numpy as np
 
+def test_stationarity(timeseries):
     timeseries.index = pd.to_datetime(timeseries.index)
     movingAverage = timeseries['value'].rolling(window=12).mean()
     movingSTD = timeseries['value'].rolling(window=12).std()
@@ -51,63 +53,75 @@ def test_stationarity(timeseries):
         print('Conclusão: A série NÃO é Estacionária.')
 
 
-def import_brent_oil_prices():
-    import requests
-    from bs4 import BeautifulSoup
-    import pandas as pd
-    from datetime import datetime
+def get_data(ticker, start_date, end_date):    
+    data = yf.download(ticker, start=start_date, end=end_date, progress=False)
+    data.reset_index(inplace=True)
+    data['Date'] = pd.to_datetime(data['Date'])
+    return data
 
-    # Função para atualizar o DataFrame com novos dados
-    def update_dataframe(df, new_data):
-        # Converte a coluna 'Data' para datetime
-        df['Data'] = pd.to_datetime(df['Data'], dayfirst=True)
-        new_data['Data'] = pd.to_datetime(new_data['Data'], dayfirst=True)
 
-        # Encontra a data mais recente no DataFrame existente
-        last_date = df['Data'].max()
+def load_data():    
+    data = pd.read_csv('./Files/oil_prices.csv', sep=';')
+    data.columns = ['Date', 'Close']
+    data['Date'] = pd.to_datetime(data['Date'])
+    return data
 
-        # Filtra as novas linhas que são mais recentes do que a última data
-        new_rows = new_data[new_data['Data'] > last_date]
 
-        # Concatena os novos dados com o DataFrame existente se houver novas linhas
-        if not new_rows.empty:
-            updated_df = pd.concat([df, new_rows], ignore_index=True)
-        else:
-            updated_df = df
+def load_gdp_data():
+    gdp_data = pd.read_csv('./Files/world_gdp.csv', sep=';')
+    gdp_data['Year'] = gdp_data['Year'].astype(int)
+    gdp_data['Global GDP'] = gdp_data['Global GDP'].astype(float)
+    return gdp_data
 
-        return updated_df
 
-    # URL do site IPEADATA
-    url = 'http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view'
+def load_sp500_data():
+    data = pd.read_csv('./Files/sp500_data.csv')
+    data.reset_index(inplace=True)
+    data['Date'] = pd.to_datetime(data['Date'])   
+    return data
 
-    # Faz uma requisição GET ao site e captura a resposta
-    response = requests.get(url)
 
-    # Verifica se a requisição foi bem sucedida
-    if response.status_code == 200:
-        # Cria um objeto BeautifulSoup para analisar o HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # Procura pela tabela no HTML analisado
-        table = soup.find('table', {'id': 'grd_DXMainTable'})
-        # Usa o pandas para ler a tabela HTML diretamente para um DataFrame
-        new_df = pd.read_html(str(table), header=0)[0]
+def create_features(df):
+    df['year'] = df['Date'].dt.year
+    df['month'] = df['Date'].dt.month
+    df['day'] = df['Date'].dt.day
+    df['dayofweek'] = df['Date'].dt.dayofweek
+    df['lag1'] = df['Close'].shift(1)
+    df['lag2'] = df['Close'].shift(2)
+    df['lag3'] = df['Close'].shift(3)
+    df['lag7'] = df['Close'].shift(7)
+    df.dropna(inplace=True)
+    return df
 
-        # Verifica se o arquivo do DataFrame existe e carrega, ou cria um novo DataFrame se não existir
-        path = '../Files/ipea.csv'
-        try:
-            existing_df = pd.read_csv(path)
-        except FileNotFoundError:
-            existing_df = new_df  # Se o arquivo não existir, considere os dados atuais como o DataFrame existente
 
-        # Atualiza o DataFrame existente com novos dados (carga incremental)
-        updated_df = update_dataframe(existing_df, new_df)
+def forecast_next_days(model, df, days=7):
+    last_row = df.iloc[-1]
+    forecast_dates = pd.date_range(start=last_row['Date'], periods=days+1)[1:]
+    forecast_df = pd.DataFrame({'Date': forecast_dates})
+    forecast_df['year'] = forecast_df['Date'].dt.year
+    forecast_df['month'] = forecast_df['Date'].dt.month
+    forecast_df['day'] = forecast_df['Date'].dt.day
+    forecast_df['dayofweek'] = forecast_df['Date'].dt.dayofweek
 
-        updated_df['Preço - petróleo bruto - Brent (FOB)'] = updated_df['Preço - petróleo bruto - Brent (FOB)']/100
+    forecasts = []
+    last_values = last_row[['Close', 'lag1', 'lag2', 'lag3', 'lag7']].values.flatten()
 
-        # Salva o DataFrame atualizado para o arquivo
-        updated_df.to_csv(path, index=False)
-
-        # Mostra as primeiras linhas do DataFrame atualizado
-        updated_df.head()
-    else:
-        print('Falha ao acessar a página: Status Code', response.status_code)
+    for i in range(days):
+        features = np.array([
+            forecast_df.loc[i, 'year'],
+            forecast_df.loc[i, 'month'],
+            forecast_df.loc[i, 'day'],
+            forecast_df.loc[i, 'dayofweek'],
+            last_values[0],
+            last_values[1],
+            last_values[2], 
+            last_values[3] 
+        ]).reshape(1, -1)
+        
+        yhat = model.predict(features)[0]
+        forecasts.append(yhat)
+        last_values = np.roll(last_values, 1)
+        last_values[0] = yhat
+   
+    forecast_df['Close'] = forecasts
+    return forecast_df
--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import yfinance as yf
 import joblib
 from datetime import datetime
 import plotly.graph_objects as go
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join('.', 'Files')))
+from utils import get_data, load_data, load_gdp_data, load_sp500_data, create_features, forecast_next_days
 
 st.set_page_config(
     page_title="Brent Oil Forecasting App",
@@ -44,59 +49,9 @@ div[data-testid="column"] {{
 </style>
 """, unsafe_allow_html=True)
 
-model = joblib.load('xgboost_model.pkl')
+model = joblib.load('./Files/xgboost_model.pkl')
 end_date = datetime.today().strftime('%Y-%m-%d')
 start_date = '1987-01-01'
-
-def get_data(ticker, start_date, end_date):    
-    data = yf.download(ticker, start=start_date, end=end_date, progress=False)
-    data.reset_index(inplace=True)
-    data['Date'] = pd.to_datetime(data['Date'])
-    return data
-
-def create_features(df):
-    df['year'] = df['Date'].dt.year
-    df['month'] = df['Date'].dt.month
-    df['day'] = df['Date'].dt.day
-    df['dayofweek'] = df['Date'].dt.dayofweek
-    df['lag1'] = df['Close'].shift(1)
-    df['lag2'] = df['Close'].shift(2)
-    df['lag3'] = df['Close'].shift(3)
-    df['lag7'] = df['Close'].shift(7)
-    df.dropna(inplace=True)
-    return df
-
-def forecast_next_days(model, df, days=7):
-    last_row = df.iloc[-1]
-    forecast_dates = pd.date_range(start=last_row['Date'], periods=days+1)[1:]
-    forecast_df = pd.DataFrame({'Date': forecast_dates})
-    forecast_df['year'] = forecast_df['Date'].dt.year
-    forecast_df['month'] = forecast_df['Date'].dt.month
-    forecast_df['day'] = forecast_df['Date'].dt.day
-    forecast_df['dayofweek'] = forecast_df['Date'].dt.dayofweek
-
-    forecasts = []
-    last_values = last_row[['Close', 'lag1', 'lag2', 'lag3', 'lag7']].values.flatten()
-
-    for i in range(days):
-        features = np.array([
-            forecast_df.loc[i, 'year'],
-            forecast_df.loc[i, 'month'],
-            forecast_df.loc[i, 'day'],
-            forecast_df.loc[i, 'dayofweek'],
-            last_values[0],
-            last_values[1],
-            last_values[2], 
-            last_values[3] 
-        ]).reshape(1, -1)
-        
-        yhat = model.predict(features)[0]
-        forecasts.append(yhat)
-        last_values = np.roll(last_values, 1)
-        last_values[0] = yhat
-   
-    forecast_df['Close'] = forecasts
-    return forecast_df
 
 st.title('Previsão de Preços do Petróleo Brent')
 st.divider()
@@ -123,8 +78,6 @@ st.write("""
 ### Dados Históricos
 """)
 
-ticker = 'BZ=F'
-
 col1, col2 = st.columns(2)
 with col1:
     container = st.container(border=True)
@@ -133,11 +86,11 @@ with col2:
     container = st.container(border=True)
     container.write(f"**Data Final:** {end_date}")
 
-data = get_data(ticker, start_date, end_date)
-data = create_features(data)
+his_data = load_data()
+his_data = create_features(his_data)
 
 fig_historical = go.Figure()
-fig_historical.add_trace(go.Scatter(x=data['Date'], y=data['Close'], mode='lines', name='Valores Reais'))
+fig_historical.add_trace(go.Scatter(x=his_data['Date'], y=his_data['Close'], mode='lines', name='Valores Reais'))
 
 fig_historical.update_layout(
     xaxis_title='Data',
@@ -153,7 +106,10 @@ st.write("""
 ### Previsão para os Próximos 7 Dias
 """)
 
-forecast_df = forecast_next_days(model, data)
+ticker = 'BZ=F'
+data_y = get_data(ticker, start_date, end_date)
+data_y = create_features(data_y) 
+forecast_df = forecast_next_days(model, data_y)
 last_7_days = forecast_df.tail(7)
 
 fig = go.Figure()
@@ -168,3 +124,84 @@ container = st.container(border=True)
 container.plotly_chart(fig, use_container_width=True)
 
 st.divider()
+gdp_data = load_gdp_data()
+
+# Insight 1: Impacto de Crises Econômicas
+st.write("""
+### Insight 1: Impacto de Crises Econômicas
+Exploramos como grandes crises econômicas, como a crise financeira de 2008, impactaram o preço do petróleo Brent. A crise de 2008, marcada pela falência do Lehman Brothers, resultou em uma queda significativa nos preços do petróleo.
+""")
+
+fig_crisis = go.Figure()
+fig_crisis.add_trace(go.Scatter(x=his_data['Date'], y=his_data['Close'], mode='lines', name='Preço Histórico'))
+fig_crisis.add_vrect(x0="2008-09-01", x1="2009-03-01", fillcolor="red", opacity=0.5, line_width=0, annotation_text="Crise Financeira 2008", annotation_position="top left")
+fig_crisis.update_layout(title='Impacto da Crise Financeira de 2008',
+                         xaxis_title='Data',
+                         yaxis_title='Preço (USD)',
+                         xaxis=dict(rangeslider=dict(visible=True)))
+
+st.plotly_chart(fig_crisis, use_container_width=True)
+
+# Insight 2: Efeitos de Situações Geopolíticas
+st.write("""
+### Insight 2: Efeitos de Situações Geopolíticas
+Situações geopolíticas, como conflitos no Oriente Médio, têm um impacto significativo nos preços do petróleo. Examinamos eventos como a Guerra do Golfo e a Primavera Árabe.
+""")
+
+fig_geopolitical = go.Figure()
+fig_geopolitical.add_trace(go.Scatter(x=his_data['Date'], y=his_data['Close'], mode='lines', name='Preço Histórico'))
+fig_geopolitical.add_vrect(x0="1990-08-01", x1="1991-02-28", fillcolor="orange", opacity=0.5, line_width=0, annotation_text="Guerra do Golfo", annotation_position="top left")
+fig_geopolitical.add_vrect(x0="2010-12-17", x1="2011-12-17", fillcolor="purple", opacity=0.5, line_width=0, annotation_text="Primavera Árabe", annotation_position="top left")
+fig_geopolitical.update_layout(title='Efeitos de Situações Geopolíticas',
+                               xaxis_title='Data',
+                               yaxis_title='Preço (USD)',
+                               xaxis=dict(rangeslider=dict(visible=True)))
+
+st.plotly_chart(fig_geopolitical, use_container_width=True)
+
+# Insight 3: Demanda Global por Energia
+st.write("""
+### Insight 3: Demanda Global por Energia
+A demanda global por energia, especialmente em países em desenvolvimento, afeta diretamente os preços do petróleo. Aqui comparamos o crescimento econômico com a variação dos preços do petróleo.
+""")
+
+# Mesclar os dados do PIB global com os dados do petróleo
+his_data['Year'] = his_data['Date'].dt.year
+merged_data = pd.merge(his_data, gdp_data, left_on='Year', right_on='Year', how='left')
+
+fig_demand = go.Figure()
+fig_demand.add_trace(go.Scatter(x=merged_data['Date'], y=merged_data['Close'], mode='lines', name='Preço do Petróleo'))
+fig_demand.add_trace(go.Scatter(x=merged_data['Date'], y=merged_data['Global GDP'], mode='lines', name='PIB Global (%)', yaxis='y2'))
+
+fig_demand.update_layout(
+    title='Demanda Global por Energia e Preços do Petróleo',
+    xaxis_title='Data',
+    yaxis_title='Preço (USD)',
+    yaxis2=dict(title='PIB Global (%)', overlaying='y', side='right'),
+    xaxis=dict(rangeslider=dict(visible=True))
+)
+
+st.plotly_chart(fig_demand, use_container_width=True)
+
+# Carregar os dados do S&P 500
+sp500_data = load_sp500_data()
+
+# Insight 4: Comparação com Outros Indicadores Econômicos
+st.write("""
+### Insight 4: Comparação com Outros Indicadores Econômicos
+Comparamos a variação do preço do petróleo com outros indicadores econômicos, como o índice S&P 500 e as taxas de câmbio, para entender melhor as correlações.
+""")
+
+# Gráfico comparando preços do petróleo com S&P 500
+fig_comparison = go.Figure()
+fig_comparison.add_trace(go.Scatter(x=his_data['Date'], y=his_data['Close'], mode='lines', name='Preço do Petróleo'))
+fig_comparison.add_trace(go.Scatter(x=sp500_data['Date'], y=sp500_data['Close'], mode='lines', name='S&P 500'))
+
+fig_comparison.update_layout(
+    title='Comparação com Outros Indicadores Econômicos',
+    xaxis_title='Data',
+    yaxis_title='Preço (USD)',
+    xaxis=dict(rangeslider=dict(visible=True))
+)
+
+st.plotly_chart(fig_comparison, use_container_width=True)
